0239c52057ad0f3c16add98c7645bc0f3e5e82e1
Julien <julien.aldon@epitech.eu>
2021-01-28 00:44:19 +0100

    fix db

--- api/database.py
+++ api/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+if os.getenv('FASTAPI_ENV') == 'prod':
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres/db"
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
