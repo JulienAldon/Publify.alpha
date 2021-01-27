d30c63cc05f5192f7f4134e65051e2a0362f19af
Julien <julien.aldon@epitech.eu>
2021-01-28 00:51:11 +0100

    fix db

--- api/database.py
+++ api/database.py
@@ -9,7 +9,7 @@ else:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
