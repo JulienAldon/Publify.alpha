30dbc0cb6f69ab5f1fd5a6c0b4b94ee630f1292d
Julien <julien.aldon@epitech.eu>
2022-05-27 18:41:15 +0200

    Remove old disposition

--- api/spotipy_driver/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = '0.0.1'
-
-from spotipy_driver.abc import Playlist, User, Track
-__all__ = 'Playlist', 'User', 'Track'
--- front/src/components/loader/index.js
+++
@@ -1,7 +0,0 @@
-import style from './style.css';
-
-function Loader({}) {
-    return <main className={style.lds}><div className={style.ldsdualring}></div></main>
-}
-
-export default Loader
